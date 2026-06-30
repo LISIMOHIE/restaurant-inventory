6eb3b532b3982ed8919ea03673f9827c3529d7e8
LISIMOHIE <mohielisi@gmail.com>
2026-06-30 16:53:54 +0300

    Fix middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -38,5 +38,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|manifest.json|sw.js|icons).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|manifest.json|sw.js|icons|api/debug).*)'],
 };
